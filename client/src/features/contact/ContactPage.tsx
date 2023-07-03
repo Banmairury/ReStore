5a551228922ef1fc9ec31b8bc01c99c73386eb54
Teerapong <banmairuruy@gmail.com>
2023-07-03 10:51:15 +0700

    End of section 4

---
+++ client/src/features/contact/ContactPage.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material"
+
+const ContactPage = () => {
+  return (
+    
+    <Typography variant="h2">ContactPage</Typography>
+  )
+}
+
+export default ContactPage
